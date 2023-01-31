7eadf87fc969cf56684480fc225e776b416ae353
JinDamanee <88868930+JinDamanee2544@users.noreply.github.com>
2023-02-01 04:41:33 +0700

    fix: Remove previous fetching code on comment

--- src/components/lessonPanel/index.tsx
+++ src/components/lessonPanel/index.tsx
@@ -1,41 +1,17 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
 import LoadingSpinner from '@components/baseComponents/LoadingSpinner';
-import Modal from './Modal';
+// import Modal from './Modal';
 import { resSource } from '@customTypes/index';
 import NodeItem from './NodeItem';
 import ViewModel from './ViewModel';
 
-// Mock fetchings
-const getResData = ({ resLink, resType }: resSource) => {
-    console.log(resLink)
-    switch (resType) {
-        case 'textNode':
-            return <p>text</p>
-        default:
-            return <p>no data</p>
-    }
-}
-
-type LessonNodeDataProps = {
+type LessonPanelProps = {
     lesson_id: string
 }
 
-type PanelContainerProps = {
-    children: React.ReactNode
-}
-
-const PanelContainer = ({ children }: PanelContainerProps) => {
-    return (
-        <div className='flex flex-col gap-4 p-6 bg-slate-100 dark:bg-gray-normal rounded-3xl  min-w-[300px]' >
-            {children}
-        </div>
-    )
-}
-
-const LessonNode = ({ lesson_id }: LessonNodeDataProps) => {
+const LessonPanel = ({ lesson_id }: LessonPanelProps) => {
     const [resSource, setResSource] = useState<resSource>({ resLink: '', resType: '' })
     const { nodes, lessonName } = ViewModel(lesson_id)
-    // const [nodeArray, setNodeArray] = useState<string[]>([])
 
     // const [isOpened, setIsOpened] = useState(false)
     // const [modalData, setModalData] = useState<any>(null)
@@ -47,16 +23,6 @@ const LessonNode = ({ lesson_id }: LessonNodeDataProps) => {
     //     }
     // }, [isOpened, resSource])
 
-    // useEffect(() => {
-    //     let LessonLoading = new Promise<Lesson>((resolve, reject) => {
-    //         const tmpValue = getLessonInformation(lesson_id)
-    //         resolve(tmpValue)
-    //     })
-    //     LessonLoading.then(value => {
-    //         setNodeArray(value.nodes)
-    //     })
-    // }, [lesson_id])
-
     if (nodes.length === 0) {
         return <LoadingSpinner />
     }
@@ -64,7 +30,7 @@ const LessonNode = ({ lesson_id }: LessonNodeDataProps) => {
     return (
         <>
             <div className='flex gap-x-20 justify-center mt-10'>
-                <PanelContainer>
+                <div className='flex flex-col gap-4 p-6 bg-slate-100 dark:bg-gray-normal rounded-3xl min-w-[300px]' >
                     <h1 className='text-2xl p-2 font-bold text-center'>{lessonName}</h1>
                     {nodes.map((node, index) => {
                         return (
@@ -81,15 +47,7 @@ const LessonNode = ({ lesson_id }: LessonNodeDataProps) => {
                             />
                         )
                     })}
-                    {/* <h1 className='text-2xl p-2 font-bold text-center'> File~~~ </h1>
-                    {nodeArray !== undefined && nodeArray.map((nodeId, index) => {
-                        return (
-                            <div key={index} className="flex gap-10 items-center" >
-                                < NodeGetHandle node_id={nodeId} setIsOpened={setIsOpened} setResSource={setResSource} />
-                            </div>
-                        )
-                    })} */}
-                </PanelContainer>
+                </div>
             </div>
             {/* 
             {   // Modal
@@ -107,4 +65,4 @@ const LessonNode = ({ lesson_id }: LessonNodeDataProps) => {
     )
 }
 
-export default LessonNode
+export default LessonPanel
